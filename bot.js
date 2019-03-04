3634e98b7011cc3b906b89b517457767b0d04791
murano61 <48194989+murano61@users.noreply.github.com>
2019-03-04 09:27:53 +0300

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,134 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const ayarlar = require('./ayarlar.json');
+const chalk = require('chalk');
+const fs = require('fs');
+const moment = require('moment');
+require('./util/eventLoader')(client);
+
+var prefix = ayarlar.prefix;
+
+const log = message => {
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] ${message}`);
+};
+
+client.commands = new Discord.Collection();
+client.aliases = new Discord.Collection();
+fs.readdir('./komutlar/', (err, files) => {
+  if (err) console.error(err);
+  log(`${files.length} komut yüklenecek.`);
+  files.forEach(f => {
+    let props = require(`./komutlar/${f}`);
+    log(`Yüklenen komut: ${props.help.name}.`);
+    client.commands.set(props.help.name, props);
+    props.conf.aliases.forEach(alias => {
+      client.aliases.set(alias, props.help.name);
+    });
+  });
+});
+
+client.reload = command => {
+  return new Promise((resolve, reject) => {
+    try {
+      delete require.cache[require.resolve(`./komutlar/${command}`)];
+      let cmd = require(`./komutlar/${command}`);
+      client.commands.delete(command);
+      client.aliases.forEach((cmd, alias) => {
+        if (cmd === command) client.aliases.delete(alias);
+      });
+      client.commands.set(command, cmd);
+      cmd.conf.aliases.forEach(alias => {
+        client.aliases.set(alias, cmd.help.name);
+      });
+      resolve();
+    } catch (e){
+      reject(e);
+    }
+  });
+};
+
+client.on('guildBanAdd' , (guild, user) => {
+  let aramızakatılanlar = guild.channels.find('name', 'aramıza-katılanlar');
+  if (!aramızakatılanlar) return;
+  aramızakatılanlar.send('https://media.giphy.com/media/8njotXALXXNrW/giphy.gif **Adalet dağıtma zamanı gelmiş!** '+ user.username +'**Bakıyorum da suç işlemiş,Yargı dağıtmaya devam** :fist: :writing_hand:  :spy:' );
+});
+
+client.load = command => {
+  return new Promise((resolve, reject) => {
+    try {
+      let cmd = require(`./komutlar/${command}`);
+      client.commands.set(command, cmd);
+      cmd.conf.aliases.forEach(alias => {
+        client.aliases.set(alias, cmd.help.name);
+      });
+      resolve();
+    } catch (e){
+      reject(e);
+    }
+  });
+};
+
+client.unload = command => {
+  return new Promise((resolve, reject) => {
+    try {
+      delete require.cache[require.resolve(`./komutlar/${command}`)];
+      let cmd = require(`./komutlar/${command}`);
+      client.commands.delete(command);
+      client.aliases.forEach((cmd, alias) => {
+        if (cmd === command) client.aliases.delete(alias);
+      });
+      resolve();
+    } catch (e){
+      reject(e);
+    }
+  });
+};
+
+client.on('message', msg => {
+  if (msg.content.toLowerCase() === 'sa') {
+      msg.reply('Aleyküm Selam Hoşgeldin !');
+}  if (msg.content.toLowerCase() === 'günaydın') {
+      msg.reply('Günaydın canım!');
+}  if (msg.content.toLowerCase() === 'seni seviyorum') {
+      msg.reply('Bende seni çok seviyorum');
+    }
+    if (msg.content.toLowerCase() === 'murat') {
+          msg.reply('koyıyımde tur at');
+        }
+        if (msg.content.toLowerCase() === 'deniz') {
+              msg.reply('Bu Discorddun en iyi moderatörü');
+            }  if (msg.content.toLowerCase() === 'build') {
+                    msg.reply('Dünyanın en iyi oyuncuların buildleri bu uygulamada.https://play.google.com/store/apps/details?id=com.buildguide.forlegends&hl=tr');
+              }
+              if (msg.content.toLowerCase() === 'takım arıyorum') {
+                      msg.reply('Takım arıyor isen takım-bulma yazışma kanalına giderek oradan aradığınız kriterlerdeki oyuncuları bulucağınıza eminiz.');
+                }
+                if (msg.content.toLowerCase() === 'sa günaydın') {
+                        msg.reply('Günaydın canımmmmm');
+                  }
+                  if (msg.content.toLowerCase() === '') {
+                          msg.reply('Günaydın canımmmmm');
+                    }
+    });
+
+client.elevation = message => {
+  if(!message.guild) {
+	return; }
+  let permlvl = 0;
+  if (message.member.hasPermission("BAN_MEMBERS")) permlvl = 2;
+  if (message.member.hasPermission("ADMINISTRATOR")) permlvl = 3;
+  if (message.author.id === ayarlar.sahip) permlvl = 4;
+  return permlvl;
+};
+
+var regToken = /[\w\d]{24}\.[\w\d]{6}\.[\w\d-_]{27}/g;
+
+client.on('warn', e => {
+  console.log(chalk.bgYellow(e.replace(regToken, 'that was redacted')));
+});
+
+client.on('error', e => {
+  console.log(chalk.bgRed(e.replace(regToken, 'that was redacted')));
+});
+
+client.login(ayarlar.token);
---
+++ events/guildMemberAdd.js
@@ -0,0 +1,5 @@
+module.exports = member => {
+    let username = member.user.username;
+    member.sendMessage('Selamlar,Değerli kankam **' + username + '** Discord Sunucuma Hoşgeldin.Bu Discord sunucusunda bütün Türkiye Moderatörleri bulunmakta haberin olsun he birde Yutbur Murat Dursun var :)');
+
+};
